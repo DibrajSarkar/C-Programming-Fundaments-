33d00a32a59320d229f94ee7f51cb6d7004eb800
DibrajSarkar <turnbackwithme@gmail.com>
2022-08-04 23:56:55 +0600

    Hello World using C programming

---
+++ hello_world.c
@@ -0,0 +1,9 @@
+#include<stdio.h> // header file
+
+int main() // one and only main function where program start
+{
+    printf("Hello World!"); // printf function use for print or show something in the screen
+
+
+    return 0;
+}
